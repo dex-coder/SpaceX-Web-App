635ae588b97b5a224cc1b8ffc4dccf9c4cbca760
dex-coder <mhdkashifkhan028@gmail.com>
2021-04-02 11:56:48 +0500

    get all launches

--- src/components/Mission/index.tsx
+++ src/components/Mission/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useMissionsInfoQuery } from './../../generated/graphql';
 
 export const MissionContainer = () => {
-    return (
-        <div>
-            
-        </div>
+    const {loading, error, data} = useMissionsInfoQuery();
+
+    if(loading)
+    return <h1>Loading...</h1>
+
+    if(error)
+    return <h1>Error</h1>
+
+    console.log(data)
+
+    return(
+        <div></div>
     )
+
 }
